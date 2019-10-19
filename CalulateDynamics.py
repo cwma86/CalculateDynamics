c695ec411e64ef1bd2e74918092eeba3ac1698e9
cwma86 <36380847+cwma86@users.noreply.github.com>
2019-10-19 15:34:44 -0600

    added functionality to receive  data via TCP and store in a circular queue

--- CalulateDynamics.py
+++ CalulateDynamics.py
@@ -11,32 +11,75 @@ class options():
         args = parser.parse_args()
         self.inputText = args.paramsFile
 
+
+class CircularQueue:
+    def __init__(self, maxQueueSize):
+        self.queueSize = maxQueueSize
+        self.objectList = [None] * maxQueueSize
+        self.head = 0
+        self.tail = 0
+
+    def dequeue(self):
+        firstPosition = self.objectList[self.head]
+        self.objectList[self.head] = None
+        if not self.head == self.tail:
+            if self.head + 1 > self.queueSize - 1:
+                self.head = 0
+            else:
+                self.head = self.head + 1
+        else:
+            self.head = 0
+            self.tail = 0        
+        return firstPosition
+
+    def enqueue(self, object):
+        if self.head == self.tail:
+            if self.objectList[self.head]:
+                # setting second value of the queue
+                if(self.tail + 1 > self.queueSize - 1):
+                    self.tail = 0
+                else:
+                    self.tail = self.tail + 1
+                self.objectList[self.tail] = object
+            else:
+                # Set initial value of the queue
+                self.objectList[self.head] = object
+        else:
+            if(self.tail + 1 > self.queueSize - 1):
+                if self.head == 0:
+                    self.dequeue()
+                self.tail = 0
+            else:
+                if self.tail + 1 == self.head:
+                    self.dequeue()
+                self.tail = self.tail + 1
+            self.objectList[self.tail] = object
+    
+
 class Handler_TCPServer(socketserver.BaseRequestHandler):
     """
     TCP Server
     """
+    queue = CircularQueue(5)
 
     def handle(self):
         self.data = self.request.recv(1024)
         print("{} sent:".format(self.client_address[0]))
         print("data %s" %self.data)
         data = self.data.decode("ASCII")
-        print(self.data)
         dataList = data.split()
-        # TODO populate queue
-        # TODO calculate average velocity 
-        # TODO calclate average acceleration
         if len(dataList) == 4:
             receivedPosition = position(dataList[0], dataList[1],
                                         dataList[2], dataList[3])
+            self.queue.enqueue(receivedPosition)
+            # TODO calculate average velocity 
+            # TODO calclate average acceleration
         else:
-            print("length = %s" %len(dataList))
-            print("dataList")
-            print(dataList)
-            print(dataList[1])
-        self.request.sendall("ACK from TCP Server".encode())
+            print("Warning Invalid length = %s" %len(dataList))
+        acknowledge = "ACK"
+        self.request.sendall(bytes(acknowledge + "\n", "ASCII"))
 
-#TODO make this class shared between the two projects
+# TODO make this class shared between the two projects
 class position:
     def __init__(self, x, y, z, time):
         self.x = x
@@ -57,58 +100,14 @@ class velocitiy:
         else:
             print("Invalid initial and final position time delta = %s" % timeDelta)
 
-class CircularQueue:
-    __init__(self, maxSize):
-        self.queueSize = maxSize
-        self.positionList = None
-        self.head = 0
-        self.tail = 0
-
-    def enqueue(self, position):
-        if self.head == self.tail:
-            if self.positionList[self.head]:
-                # setting second value of the queue
-                if(self.tail + 1 > self.queueSize - 1):
-                    self.tail = 0
-                else:
-                    self.tail++
-                self.positionList[self.tail] = position
-            else:
-                # Set initial value of the queue
-                self.positionList[self.head] = position
-
-        else:
-            if(self.tail + 1 > self.queueSize - 1):
-                self.tail = 0
-            else:
-                self.tail++
-            if self.head == self.tail:
-                # Queue is full replace oldest object
-                if self.head + 1 > self.queue size - 1:
-                    self.head = 0
-                else:
-                    self.head++
-            self.positionList[self.tail] = position
-
-
-
-            else:
-                self.positionList[self.head] = position
-    
-    def dequeue(self, position):
 
 def main():
+    # TODO make host/port configurable
     inputArgs = options()
-    # TODO start TCP server
     HOST = "127.0.0.1"
     PORT = 9999
     tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
     tcp_server.serve_forever()
-    # TODO receive data via tcp 
-    # TODO store positon
-    # TODO calculate velocity
-    # TODO calculate average velocity (circular queue)
-    # TODO calculate acceleration
 
 if __name__ == "__main__":
     main()
